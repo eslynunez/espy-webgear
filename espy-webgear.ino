bd1bd4663d4dba5a7f3359d5924ba01b60757765
eslynunez <=>
2016-10-18 23:02:40 -0400

    base ESP8266 iot-webgear firmware

--- espy-webgear.ino
+++ espy-webgear.ino
@@ -10,8 +10,8 @@
 #include <ESP8266WebServer.h>
 #include <ESP8266mDNS.h>
 
-const char *ssid = "CleverWillow";
-const char *password = "BMUeVY7TJX";
+const char *ssid = "SSID";
+const char *password = "PASSWORD";
 
 ESP8266WebServer server ( 80 );
 
